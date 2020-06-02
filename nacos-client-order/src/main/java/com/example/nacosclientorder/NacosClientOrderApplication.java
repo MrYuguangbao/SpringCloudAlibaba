db6796724daa1a84032c6653aa39b003dbd34287
arist <18271462455@163.com>
2020-06-02 10:09:52 +0800

    初始化springcloudalibaba maven父工程，nacos客户端

---
+++ nacos-client-order/src/main/java/com/example/nacosclientorder/NacosClientOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.nacosclientorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosClientOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosClientOrderApplication.class, args);
+    }
+
+}
---
+++ nacos-client-order/src/test/java/com/example/nacosclientorder/NacosClientOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nacosclientorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosClientOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
